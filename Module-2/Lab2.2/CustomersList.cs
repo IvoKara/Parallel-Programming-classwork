c8fc5039423ac4a67bb081f2eb0ec7ab636832b9
IvoKara <ivo.i.karaneshev@gmail.com>
2023-10-26 09:09:45 +0300

    feat: create Customer class and Customers list

---
+++ Module-2/Lab2.2/CustomersList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab2._2
+{
+    class CustomersList
+    {
+        public List<Customer> Customers { get; private set; } = new List<Customer>() { 
+            new Customer(1, "Ivo", "Sofia", 4),
+            new Customer(2, "Pesho", "Varna", 2),
+            new Customer(3, "Alex", "Plovdiv", 3),
+            new Customer(4, "Max", "Stara Zagora", 5),
+            new Customer(5, "Kristian", "Sofia", 10),
+        };
+
+        public async Task<Customer?> FindById(int id)
+        {
+            Customer? customer = Customers.Find( c => c.Id == id);
+            await Task.Delay(500);
+            return customer;
+        }
+    }
+}
